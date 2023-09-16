a107eb559426560b15bd5bee40069740f92ff513
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-16 19:39:55 -0300

    Projeto_Django_templates_boas_praticas: Iniciando entendimento de ORM de Django

--- Projeto_Django_templates_boas_praticas/galeria/models.py
+++ Projeto_Django_templates_boas_praticas/galeria/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
-    foto = models.CharField(max_length=100, null=False, blank=False)
+    foto = models.TextField(max_length=100, null=False, blank=False)
 
     def __str__(self) -> str:
         return f'Nome: {self.nome} - Legenda: {self.legenda} - Foto: {self.foto}'
