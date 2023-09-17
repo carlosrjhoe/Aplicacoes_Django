761ad14bfe15e2918246ebe191e89bdf3fe8ea03
Carlos Roberto <carlosrjhoe@gmail.com>
2023-09-16 21:47:24 -0300

    Projeto_Django_templates_boas_praticas: Iniciando configuração do Admin

--- Projeto_Django_templates_boas_praticas/galeria/admin.py
+++ Projeto_Django_templates_boas_praticas/galeria/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Fotografia
 
 # Register your models here.
+class GaleriaAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nome', 'legenda')
+    admin.site.register(Fotografia)
--- Projeto_Django_templates_boas_praticas/galeria/models.py
+++ Projeto_Django_templates_boas_praticas/galeria/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Fotografia(models.Model):
     nome = models.CharField(max_length=100, null=False, blank=False)
     legenda = models.CharField(max_length=150, null=False, blank=False)
-    foto = models.TextField(max_length=100, null=False, blank=False)
+    foto = models.ImageField(upload_to='post_img/%Y/%m/%d', blank=True, null=True, verbose_name='imagem')
 
     def __str__(self) -> str:
-        return f'Nome: {self.nome} - Legenda: {self.legenda} - Foto: {self.foto}'
+        return f'Nome: {self.nome} - Legenda: {self.legenda}'
