72ff43d7cdcc9764bd512f1d3dcd35b1458de60b
Carlos Roberto <carlosrjhoe@gmail.com>
2023-10-04 21:13:03 -0300

    Projeto_Django_templates_boas_praticas: Injetando o contexto dos models na views para o template ficar mais dinâmico

--- Projeto_Django_templates_boas_praticas/galeria/models.py
+++ Projeto_Django_templates_boas_praticas/galeria/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Cavaleiro(models.Model):
+    nome = models.CharField(max_length=50, null=False, blank=False)
+    constelacao = models.CharField(max_length=50, null=False, blank=False)
+    descricao = models.TextField(null=False, blank=False)
+
+    objects = models.Manager()
+
+    def __str__(self) -> str:
+        return f'{self.nome} - {self.constelacao}'
--- Projeto_Django_templates_boas_praticas/galeria/views.py
+++ Projeto_Django_templates_boas_praticas/galeria/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
+from .models import Cavaleiro
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    # Primeiro, buscamos os cavaleiros
+    cavaleiros = Cavaleiro.objects.all()
+    
+    # Incluímos no contexto
+    context = {
+        'cavaleiros': cavaleiros
+    }
+
+    # Retornamos o template para listar os cavaleiros
+    return render(request, 'galeria/index.html', context)
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
