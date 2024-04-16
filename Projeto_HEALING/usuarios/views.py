f027c84f0e6a1d8d16a07205943b763ed0085231
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-16 20:59:03 -0300

    Projeto_Pystack_Week: Emplementando sistemas de messagens de erro

--- Projeto_HEALING/usuarios/views.py
+++ Projeto_HEALING/usuarios/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.messages import constants
+from django.contrib import messages
 
 # Create your views here.
 def cadastro(request):
@@ -12,14 +14,17 @@ def cadastro(request):
         confirmar_senha = request.POST.get('confirmar_senha')
         
         if senha != confirmar_senha:
+            messages.add_message(request, constants.ERROR, 'A senha e o confirmar senha deve ser iguais!')
             return redirect('/usuarios/cadastro/')
 
         if len(senha) < 3:
+            messages.add_message(request, constants.ERROR, 'A senha deve ter mais de 6 digitos!')
             return redirect('/usuarios/cadastro/')
 
         users = User.objects.filter(username=username)
         
         if users.exists():
+            messages.add_message(request, constants.ERROR, 'Usuário ja cadastrado!')
             return redirect('/usuarios/cadastro/')
 
         user = User.objects.create_user(
