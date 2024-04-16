792c3ec0d33fe00ffb890dd3ddc193623beb43ae
CarlosRJhoe <carlosrjhoe@gmail.com>
2024-04-16 20:43:45 -0300

    Projeto_Pystack_Week: Criando funcionalidade da Views cadastro.

--- Projeto_HEALING/usuarios/views.py
+++ Projeto_HEALING/usuarios/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
 
 # Create your views here.
 def cadastro(request):
-    return render(request, 'usuarios/cadastro.html')
+    if request.method == 'GET':
+        return render(request, 'usuarios/cadastro.html')
+    elif request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        senha = request.POST.get('senha')
+        confirmar_senha = request.POST.get('confirmar_senha')
+        
+        if senha != confirmar_senha:
+            return redirect('/usuarios/cadastro/')
+
+        if len(senha) < 3:
+            return redirect('/usuarios/cadastro/')
+
+        users = User.objects.filter(username=username)
+        
+        if users.exists():
+            return redirect('/usuarios/cadastro/')
+
+        user = User.objects.create_user(
+            username = username,
+            email = email,
+            password = senha
+        )
